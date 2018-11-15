15720028a1495e7d6213e92ac2c7b9f91aad5004
hahahalori <ldkregar@yahoo.com>
2018-11-15 20:17:26 +0000

    reads all genscan csv files and produces single matrix

---
+++ combineGenscanResults.R
@@ -0,0 +1,32 @@
+# combine all genscan outputs into 1 single matrix
+
+allFilesCsv <- list.files("genscan/", pattern = "*.csv")
+
+masterTable <- data.frame()
+
+for(i in 1:length(allFilesCsv)){
+  #i <- 5
+  #inputFile <- "genscan/gs_scaffold_6473_part_1.fasta.out.csv"
+  
+  inputFile <- paste0("genscan/", allFilesCsv[i])
+  firstLine <- readLines(con = inputFile, n = 1)
+  tableOfExons <- read.table(file = inputFile, skip = 1, header = TRUE,
+                             sep = ",")
+  
+  scfIdTrimBegin <- substring(inputFile, 12, nchar(inputFile))
+  scfTrimEnd <- substring(scfIdTrimBegin, 1, nchar(scfIdTrimBegin) - 14)
+  if(dim(tableOfExons)[1] == 0){
+    
+    asDf <- data.frame(matrix(NA,ncol = 13))
+    colnames(asDf) <- c("Gn.Ex","Type","S",".Begin","...End",".Len","Fr","Ph",
+                                      "I.Ac", "Do.T", "CodRg", "P....", "Tscr..")
+    dataAll <- cbind(name = scfTrimEnd, description = firstLine, asDf)  
+    
+  }else{
+    dataAll <- cbind(name = scfTrimEnd, description = firstLine, tableOfExons)
+  }
+  
+  masterTable <- rbind(masterTable, dataAll)
+  
+}
+
