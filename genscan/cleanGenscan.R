35fa9f892f1c356a07ff7afa9a95e779e0473e62
hahahalori <ldkregar@yahoo.com>
2018-11-16 17:20:15 +0000

    Update cleanGenscan.R

--- genscan/cleanGenscan.R
+++ genscan/cleanGenscan.R
@@ -12,7 +12,7 @@ parseAndReturnList <- function(x){
   # first element is name of scaffold and info
   # second element is dataframe of found proteins
   
-  #x <- scf_manyHits
+  #x <- scf_oneHit
   nameOfOutput <- x[[3]]
   
   # checks in no exons found
@@ -25,42 +25,77 @@ parseAndReturnList <- function(x){
   #extracts only information to be used in table
   chrOfHits <- x[c(9, 12:lastLineOfTable)]
   
-  chr_clean <- chrOfHits[chrOfHits != ""] 
-  
-  # c(1:5), c(7:10), c(12), c(14:19), c(21:26), c(28:31), c(33:34), c(36:37), c(39:42), c(44:47), c(49:53), c(55:59), c(61:66)
-  
-  # incides of text that is useful
-  # all genscan tables have 66 char per table row
-  indicesOfGenscanTable <- list(c(1:5), c(7:10), c(12), c(14:19), c(21:26), c(28:31), c(33:34), c(36:37), c(39:42), c(44:47), c(49:53), c(55:59), c(61:66))
-  
-  # preallocate for result
-  listOfRows <- vector(mode = "list", length = length(chr_clean)+1)
-  
-  # go through every line of input and return only text from the indices that are relevant
-  for(i in 1:length(chr_clean)){
-    #i <- 1
-    returnText <- lapply(indicesOfGenscanTable, function(indices, tableLine){
-      #indices <- indicesOfGenscanTable[[1]]
-      #tableLine <- find_40
+  if(nchar(x[12]) == 66){
+    
+    chr_clean <- chrOfHits[chrOfHits != ""] 
+    
+    
+    
+    
+    # c(1:5), c(7:10), c(12), c(14:19), c(21:26), c(28:31), c(33:34), c(36:37), c(39:42), c(44:47), c(49:53), c(55:59), c(61:66)
+    
+    # incides of text that is useful
+    # all genscan tables have 66 char per table row
+    #indicesOfGenscanTable <- list(c(1:5), c(7:10), c(12), c(14:19), c(21:26), c(28:31), c(33:34), c(36:37), c(39:42), c(44:47), c(49:53), c(55:59), c(61:66))
+    
+    # preallocate for result
+    listOfRows <- vector(mode = "list", length = length(chr_clean))
+    
+    # go through every line of input and return only text from the indices that are relevant
+    for(i in 1:length(chr_clean)){
+      #i <- 1
+      stringNoSpace <- strsplit(chr_clean[i], ' +')[[1]]
+      stringNoSpaceNoEmpty <- stringNoSpace[stringNoSpace != ""]
+      listOfRows[[i]] <- stringNoSpaceNoEmpty
       
-      splitted <- strsplit(tableLine, "")[[1]]
-      chars <- splitted[indices]
-      chars_noEmpty <- chars[chars != " "]
-      return(paste0(chars_noEmpty, collapse = ""))
       
-    }, tableLine = chr_clean[i])
-    listOfRows[[i]] <- returnText
+      
+      
+    #   returnText <- lapply(indicesOfGenscanTable, function(indices, tableLine){
+    #     #indices <- indicesOfGenscanTable[[1]]
+    #     #tableLine <- find_40
+    #     
+    #     splitted <- strsplit(tableLine, "")[[1]]
+    #     chars <- splitted[indices]
+    #     chars_noEmpty <- chars[chars != " "]
+    #     return(paste0(chars_noEmpty, collapse = ""))
+    #     
+    #   }, tableLine = chr_clean[i])
+    #   listOfRows[[i]] <- returnText
+    }
+    
+    # store in dataframe
+    
+    mtrAll <- lapply(listOfRows, function(oneRow){
+      if(length(oneRow) != 13){
+        return(c(oneRow[1:6], rep(NA, 6), oneRow[7]))
+      }else{
+        return(oneRow)
+      }
+      
+    })
+    #mtrx <- matrix(unlist(listOfRows), ncol=13, byrow=TRUE)
+    #df <- as.data.frame(mtrx, stringsAsFactors = F)
+    # make colnames out of first row
+    dfAll <- do.call("rbind", mtrAll)
+    dfAll_df <- as.data.frame(dfAll, stringsAsFactors = F)
+    colnames(dfAll_df) <- dfAll_df[1,]
+    dfAllNice <- dfAll_df[-1,]
+    #dfAllNice2 <- as.data.frame(dfAllNice, col.names = names(dfAllNice))
+    
+    
+    #dfAllNice_df_num <- dfAllNice_df
+    dfAllNice[,c(4:13)] <- sapply(dfAllNice[,c(4:13)], function(singleCol){
+      as.numeric(singleCol)
+    })
+    
+    return(list(nameOfOutput, dfAllNice))
+    
+  }else{
+    #cat(nameOfOutput)
+    return(list(nameOfOutput, NA))
   }
-  
-  # store in dataframe
-  mtrx <- matrix(unlist(listOfRows), ncol=13, byrow=TRUE)
-  df <- as.data.frame(mtrx, stringsAsFactors = F)
-  # make colnames out of first row
-  colnames(df) <- df[1,]
-  df <- df[-1,]
-  df2 <- as.data.frame(lapply(df, type.convert), stringsAsFactors = F)
-  
-  return(list(nameOfOutput, df2))
+
 }
 
 #t1 <- parseAndReturnList(scf_noHits)
@@ -69,7 +104,9 @@ parseAndReturnList <- function(x){
 
 # read every file in directory
 # I had all genscan results in the directory genscan and they started with gs_
-allFiles <- list.files("genscan/", pattern = "gs_*")
+allFiles <- list.files("genscan/", pattern = ".fasta.out$")
+
+#allFiles <- list.files("genscan/gs_part/")
 
 # for every file
 for(i in 1:length(allFiles)){
@@ -78,7 +115,7 @@ for(i in 1:length(allFiles)){
   
   # parse the file
   parsed <- parseAndReturnList(oneGenscanOutput)
-  
+  #cat(oneGenscanOutput)
   # write csv file
   outputFile <- paste0("genscan/", allFiles[i], ".csv")
   
@@ -93,5 +130,10 @@ for(i in 1:length(allFiles)){
   }
   
 }
+
+
+
+
+
 
 
